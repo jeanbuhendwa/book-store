ad7f015e9a00c55c0b4b4d8bff79c5d92a6d220c
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-04-06 17:53:36 +0200

    Update NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   const links = [
@@ -6,15 +8,20 @@ const NavBar = () => {
     { path: 'categories', text: 'Categories' },
   ];
   return (
-    <nav>
-      <p>BookStore CMS</p>
-      <ul>
-        {links.map((link) => (
-          <li key={link.text}>
-            <NavLink to={link.path}>{link.text}</NavLink>
-          </li>
-        ))}
-      </ul>
+    <nav className="navigationMenu">
+      <div>
+        <p>BookStore CMS</p>
+        <ul>
+          {links.map((link) => (
+            <li key={link.text}>
+              <NavLink to={link.path} className="navLink">
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <FontAwesomeIcon icon={faUser} className="userIcon" />
     </nav>
   );
 };
