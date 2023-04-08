a1b75398c57a1b8dc6a6b15ba32cfafce9e154ee
Jean Jacques Buhendwa <jeanjacquesirenge40@gmail.com>
2023-04-08 21:36:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* connect the redux to the app  */}
+      {/* connect the redux
       <Provider store={Store}>
         <App />
       </Provider>
